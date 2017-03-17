61365784e2125ece4de8eb0130d4186f304fc48a
Leonardo Steffen <leosteffen84@gmail.com>
2017-03-16 18:31:43 -0700

    extracts todo item from app jsx to its own todoList component | use spread operator to pass all the todo props to the list component

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import {TodoItem} from './TodoItem';
 
 export const TodoList = (props) => {
   return (
     <div className="Todo-List">
       <ul>
-        {props.todos.map(todo =>
-          <li key={todo.id}>
-            <input type="checkbox" defaultChecked={todo.isComplete}/>{todo.name}
-          </li>)}
+        {props.todos.map(todo => (
+          <TodoItem key={todo.id}
+            {...todo}
+            />
+          )
+        )}
       </ul>
     </div>
   )
