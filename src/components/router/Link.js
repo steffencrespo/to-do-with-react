f8f206bc20b3cddab9cca809e552a53d4ae66f8a
Leonardo Steffen <leosteffen84@gmail.com>
2017-03-28 11:41:03 -0700

    adds handleClick function on Link component to handle routes passed by the footer component | adds a TO property to Footer component with the routes each link should pass to the Link component to route

--- src/components/router/Link.js
+++ src/components/router/Link.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
 
 export class Link extends Component {
+  handleClick = (evt) => {
+    evt.preventDefault();
+    history.pushState(null,'', this.props.to)
+  }
   render() {
-    return <a href='#'>{this.props.children}</a>
+    return <a href="#" onClick={this.handleClick}>{this.props.children}</a>
   }
 }
